efd9fb109bc19ee203b0399a76439d71c15421a5
Alexander Gamble <alexander.gamble@outlook.com>
2014-10-04 18:37:08 +0100

    test

--- twilio.js
+++ twilio.js
@@ -9,7 +9,7 @@ http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/xml'});
     res.end(twiml.toString());
 
-}).listen(80);
+}).listen( process.env.PORT || 1337);
 
 console.log('TwiML servin\' server running at http://127.0.0.1:1337/');
 
