50793abf5c05a987486f866c85e1e5a13deb7a45
zahra <zahratee@gmail.com>
2014-10-04 15:10:20 +0100

    Minor edits + explanation of bitcoins added to Transaction.js

--- app/models/transaction.js
+++ app/models/transaction.js
@@ -8,14 +8,24 @@ db.once('open', function callback () {
 });
 
 var transactionSchema = mongoose.Schema({
-	inputs: [{
+	inputs: [{ // outputs of prev transaction
 		address: String
 	}]
 	outputs: [{
-		address: String
+		address: String,
+		//need to check if output matches a user 
 	}]
 	transactionHash: String
 });
 
+var Transaction = mongoose.model('Transaction', transactionSchema);
 
-var User = mongoose.model('Transaction', transactionSchema);
+
+//when blockchain sends an event where the output = one of our inputs... create a new transaction
+//transaction holds unknown input and output = one of the user addresses
+
+
+//301: send 2BTC to 302 - check if they have 2BTC.
+//If so, create a new transaction with input containing output of previous transaction
+// and take transaction hash to newly generated random address
+// and take private key of prev transaction and sign (within node)
